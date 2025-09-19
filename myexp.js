e58434b83c43a3aa35d9ebb766c95e0990ac804d
anurag-vns <129647178+anurag-vns@users.noreply.github.com>
2025-09-19 23:24:11 +0530

    express.js
    hello world

--- myexp.js
+++ myexp.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 const port= 3000;
 app.get('/', (req, res) => {
-    res.send('Hello world');
+    res.send('Hello my world');
 });
 
 app.listen(port,()=>{
