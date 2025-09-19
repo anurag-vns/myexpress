481c4f3eb6b6f44f53db6eae34fc2085dd260210
anurag-vns <129647178+anurag-vns@users.noreply.github.com>
2025-09-19 23:22:23 +0530

    express with multiple router

--- myexprouter.js
+++ myexprouter.js
@@ -5,6 +5,14 @@ app.get("/", (req, res) => {
     res.send("welcome on Homepage");
 });
 
+app.get("/about", (req, res) => {
+    res.send("welcome on about page");
+});
+
+app.get("/contact", (req, res) => {
+    res.send("welcome on contact page");
+});
+
 app.listen(port,()=>{
     console.log(`localhost:${port}`);
 });
