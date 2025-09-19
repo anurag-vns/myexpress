7122b70cc287e293b481b54e99133de17f4dd52c
anurag-vns <129647178+anurag-vns@users.noreply.github.com>
2025-09-19 22:38:12 +0530

    Initial commit

---
+++ myexp.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+const port= 3000;
+app.get('/', (req, res) => {
+    res.send('Hello world');
+});
+
+app.listen(port,()=>{
+    console.log(`localhost:${port}`);
+});
