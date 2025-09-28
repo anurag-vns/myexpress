0f91c3401f6b2e59f0c5c52f55ce944e3c6498bf
anurag-vns <129647178+anurag-vns@users.noreply.github.com>
2025-09-28 22:19:20 +0530

    create server node-js
    send object as response

--- mynodeserver.js
+++ mynodeserver.js
@@ -1,7 +1,11 @@
 const http= require('http');
 const server= http.createServer((req,res)=>{
-res.writeHead(200,{"content-type":"text/plain"})
-res.end("learnin node js");
+    const obj={
+        name:"Anurag",
+        surname:"shukla"
+    }
+res.writeHead(200,{"content-type":"application/json"})
+res.end(JSON.stringify(obj));
 })
 server.listen(4000,()=>{
     console.log("server started");
